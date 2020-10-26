288e3971d1510a68cf748b8178361fe4aecd36c1
CyberSander <cyber.sander777@gmail.com>
2020-10-26 13:54:36 +0200

    added gui

--- src/WeatherObserver.kt
+++ src/WeatherObserver.kt
@@ -8,12 +8,18 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.util.*
 
-class Weather {
+class WeatherObserver {
     private var token: String = ""
     var location: String = ""
     var weather: JSONTree = JSONTree()
-    constructor(config: JSONTree) {
-        this.token = config["weather-token"].getValue()
+
+    constructor(configFile: File) {
+        var content = ""
+        val read = Scanner(configFile)
+        while (read.hasNext()) {
+            content += read.next()
+        }
+        this.token = MyString(content).getTree()["weather-token"].getValue()
     }
 
     constructor(configFile: File, location: String) {
@@ -28,6 +34,7 @@ class Weather {
 
     fun update() {
         val url = URL("http://api.weatherstack.com/current?access_key=${this.token}&query=${this.location}")
+//        val url = URL("http://192.168.1.56/weatherAPI/weatherforecast?city=${this.location}")
         val con: HttpURLConnection = url.openConnection() as HttpURLConnection
         con.requestMethod = "GET"
         val inStream = InputStreamReader(con.inputStream)
@@ -35,7 +42,10 @@ class Weather {
         for (i in inStream.readLines()) {
             buff += i
         }
-        this.weather = MyString(buff).getTree()
+        println(buff)
+        val weatherMyStr = MyString(buff)
+        this.weather = weatherMyStr.getTree()
+        println(weatherMyStr.str)
     }
 
     operator fun get(index: String): Node {
--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,44 @@
 import java.io.File
+import javax.swing.*
+
+class App(val observer: WeatherObserver) : JFrame() {
+    init {
+        initUI()
+    }
+
+    private fun initUI() {
+        layout = null
+        isResizable = false
+        title = "Simple example"
+        setSize(500, 800)
+        setLocationRelativeTo(null)
+        defaultCloseOperation = EXIT_ON_CLOSE
+        val cityField = JTextField("location name")
+        val weatherField = JTextArea()
+        weatherField.isEditable = false
+        weatherField.setBounds(20, 100, 460, 400)
+        add(weatherField)
+        cityField.setBounds(175, 20, 150, 30)
+        add(cityField)
+        val showButton = JButton("Show Weather")
+        showButton.setBounds(190, 60, 120, 20)
+        showButton.addActionListener {
+            observer.location = cityField.text
+            observer.update()
+            var print = ""
+            val current = observer["current"].getNode()
+            for (i in current.keys()) {
+                print += (i + ": " + current[i].value + "\n")
+                println(i + ": " + current[i].value)
+            }
+            weatherField.text = print
+        }
+        add(showButton)
+
+    }
+}
 
 fun main() {
-    val weather = Weather(File("configs"), "Kyyiv")
-    weather.update();
-    val current = weather["current"].getNode()
-    for(i in current.keys()) {
-        println(i + ": " + current[i].value)
-    }
+    val test = App(WeatherObserver(File("configs")))
+    test.isVisible = true
 }
